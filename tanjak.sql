ff27a0ff8f7e6b1e4d1527e5ce87f3c651369940
Toniyahyama <toniyahya451@gmail.com>
2025-07-15 22:31:51 +0700

    Update tanjak.sql

--- tanjak.sql
+++ tanjak.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2025 at 06:23 AM
+-- Generation Time: Jul 15, 2025 at 05:29 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -32,6 +32,7 @@ CREATE TABLE `umkm` (
   `nama_umkm` varchar(100) NOT NULL,
   `kategori_umkm` enum('Kuliner','Jasa','Toko Kelontong & Peralatan','Lainnya') NOT NULL,
   `alamat` text DEFAULT NULL,
+  `rw` varchar(10) DEFAULT NULL,
   `no_telp` varchar(20) DEFAULT NULL,
   `nama_pemilik` varchar(100) DEFAULT NULL,
   `deskripsi` text DEFAULT NULL,
@@ -52,6 +53,13 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID_user`, `username`, `password`, `created_at`) VALUES
+(2, 'admin', '$2y$10$WIDVqIYQfhWNWvcd9ylJ1u0mWrqCDonfZbSWPay4W8zYw6zCcX0wK', '2025-07-15 21:33:39');
 
 --
 -- Indexes for dumped tables
@@ -84,7 +92,7 @@ ALTER TABLE `umkm`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
